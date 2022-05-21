bfd049f271aa7c3148363da1d868e57e00d4e76e
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-05-21 12:28:39 +0300

    Второй коммит

--- src/Main.java
+++ src/Main.java
@@ -100,5 +100,18 @@ public class Main {
                 System.out.println("Некорректный месяц" + monthNumber);
         }
     }
+    public static void main(String[] args) {
+        int age = 23;
+        int salary = 58_000;
+        if (age>=23)
+            //"Мы предоставляем лимит в размере 3 зарплат."
+            System.out.println( "Мы готовы выдать вам кредитную карту с лимитом " +salary*3+  " рублей");
+        else if (age<=23)
+            System.out.println("Мы предоставляем лимит в размере 2 зарплат.");
+        else if (age>=80)
+            System.out.println("Мы увеличиваем лимит в 1.5 раза.");
+    }
+}
+
 
 
